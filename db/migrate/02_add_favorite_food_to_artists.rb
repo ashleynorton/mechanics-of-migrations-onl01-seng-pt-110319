51e8da5d3b584faf3ab686eb544506d16ae98d5b
Ashley Norton <nortonashleym@gmail.com>
2020-02-25 22:18:21 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -4,4 +4,4 @@ class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
   end 
 end 
 
-#
+# We just told Active Record to add a column to the artists table called favorite_food and that it will contain a string.
